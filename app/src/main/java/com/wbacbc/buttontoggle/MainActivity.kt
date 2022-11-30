52d81b3d8e42c83e83381b4152844bba649d2330
SongYinan <312966673@qq.com>
2022-11-30 16:04:25 +0800

    Android MaterialButtonToggleGroup

--- app/src/main/java/com/wbacbc/buttontoggle/MainActivity.kt
+++ app/src/main/java/com/wbacbc/buttontoggle/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.wbacbc.buttontoggle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wbacbc.buttontoggle.databinding.ActivityMainBinding
 
+
+/**
+ * https://its301.com/article/cunchi4221/107477589
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked -> }
     }
 }
