1f0d4d507596a3a3f4ad13d7547f10d738d16006
SongYinan <312966673@qq.com>
2022-11-30 14:59:55 +0800

    GitHub SegmentedButton

--- app/src/main/java/com/wbacbc/buttontoggle/MainActivity.kt
+++ app/src/main/java/com/wbacbc/buttontoggle/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.wbacbc.buttontoggle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.addisonelliott.segmentedbutton.SegmentedButtonGroup.OnPositionChangedListener
+import com.wbacbc.buttontoggle.databinding.ActivityMainBinding
+
+
+/**
+ * https://github.com/addisonElliott/SegmentedButton
+ */
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonGroupLordOfTheRings.onPositionChangedListener = OnPositionChangedListener {
+
+            // Get current position
+            val position = binding.buttonGroupLordOfTheRings.position
+            // Handle stuff here
+            Log.d("MainActivity", "$position")
+        }
+
+
     }
 }
